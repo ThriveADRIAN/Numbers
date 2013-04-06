b5050849ebd5a1869e9bc505694ef16a9c458643
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-06 18:38:57 -0400

    Exercises and Challenge Completed

--- Numbers/main.c
+++ Numbers/main.c
@@ -7,12 +7,28 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    unsigned long x = 255;
+    printf("x is %lu.\n", x);
+    printf("In octal, x is %lo.\n", x);
+    printf("In hexadecimal, x is %lx.\n", x);
+    
+    printf("3 * 3 + 5 * 2 = %d\n", 3 * 3 + 5 * 2);
+    printf("11 / 3 = %d remainder of %d\n", 11 / 3, 11 % 3);
+    printf("11 / 3.0 = %f\n", 11 / (float)3);
+    printf("The absolute value of -5 is %d\n", abs(-5));
+    
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    
+    double radian = 1;
+    printf("The sine of 1 radian is %.3f\n.", sin(radian));
+    
     return 0;
 }
 
